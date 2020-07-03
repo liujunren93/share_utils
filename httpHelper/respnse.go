8c554b05b3938a352120c3a251ab64e7f7f10980
768712894 <768712894@qq.com>
2020-07-03 22:57:22 +0800

    de

--- httpHelper/respnse.go
+++ httpHelper/respnse.go
@@ -1,5 +1,9 @@
 package httpHelper
 
+type option interface {
+	getStatus() (int32, string)
+}
+
 // 响应
 type HttpResponse struct {
 	Code int32       `json:"code"`
@@ -7,21 +11,54 @@ type HttpResponse struct {
 	Data interface{} `json:"data"`
 }
 
-const (
-	BindingErr        int32 = 4001 //数据错误
-	InternalServerErr int32 = 5000
-	DataErr           int32 = 5001
-)
+// 数据校验不通过
+type BindingError string
 
-func (HttpResponse) ResSuccess(msg string, data interface{}) *HttpResponse {
-	if msg == "" {
-		msg = "ok"
+func (e BindingError) getStatus() (int32, string) {
+	if e != "" {
+		e = "数据校验不通过"
 	}
-	return newResponse(200, msg, data)
+	return 4001, string(e)
 }
 
-func (HttpResponse) ResError(code int32, msg string, data interface{}) *HttpResponse {
-	return newResponse(code, msg, data)
+type InternalServerError string
+
+func (e InternalServerError) getStatus() (int32, string) {
+	if e != "" {
+		e = "Internal Server Error"
+	}
+	return 5000, string(e)
+}
+
+type DataError string
+
+func (e DataError) getStatus() (int32, string) {
+	if e != "" {
+		e = "服务器数据错误"
+	}
+	return 5000, string(e)
+}
+
+type OtherError string
+
+func (e OtherError) getStatus() (int32, string) {
+	if e != "" {
+		e = "未知错误"
+	}
+	return 5000, string(e)
+}
+
+func (HttpResponse) ResSuccess(o option, data interface{}) *HttpResponse {
+	status, msg := o.getStatus()
+	return newResponse(status, msg, data)
+}
+
+func (HttpResponse) ResError(o option, others ...interface{}) *HttpResponse {
+	status, msg := o.getStatus()
+	if others[0] != nil {
+		status = others[0].(int32)
+	}
+	return newResponse(status, msg, others[1])
 }
 
 func newResponse(code int32, msg string, data interface{}) *HttpResponse {
