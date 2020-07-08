55231ce724e98adfb846a14f5d255fa14ae6e65a
768712894 <768712894@qq.com>
2020-07-08 16:39:41 +0800

    de

--- wrapper/breaker/hystrix.go
+++ wrapper/breaker/hystrix.go
@@ -2,37 +2,28 @@ package breaker
 
 import (
 	"context"
-	"fmt"
 	"github.com/afex/hystrix-go/hystrix"
 	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/registry"
 )
 
-type clientWrapper struct {
-	client.Client
-}
+func NewClientWrapper(maxConcurrent, Timeout int) client.CallWrapper {
 
-func (c *clientWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
-
-	return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
-		return c.Client.Call(ctx, req, rsp, opts...)
-	}, nil)
-}
-
-func NewClientWrapper(maxConcurrent, Timeout int) client.Wrapper {
-	fmt.Println(111111)
-	if maxConcurrent == 0 {
-		hystrix.DefaultMaxConcurrent = 10
-	} else {
+	if maxConcurrent != 0 {
 		hystrix.DefaultMaxConcurrent = maxConcurrent
 	}
-	if Timeout == 0 {
-		hystrix.DefaultTimeout = 1000
-	}else{
+	if Timeout != 0 {
 		hystrix.DefaultTimeout = Timeout
 	}
-
-
-	return func(c client.Client) client.Client {
-		return &clientWrapper{c}
+	hystrix.DefaultVolumeThreshold = 2
+	return func(callFunc client.CallFunc) client.CallFunc {
+		return func(ctx context.Context, node *registry.Node, req client.Request, rsp interface{}, opts client.CallOptions) error {
+			return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
+				return callFunc(ctx, node, req, rsp, opts)
+			}, func(err error) error {
+				return err
+			})
+		}
 	}
+
 }
