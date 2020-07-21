86e7e18f5587c1cd14b0c794f5737d538983794c
768712894 <768712894@qq.com>
2020-07-21 11:43:53 +0800

    de

--- helper/string.go
+++ helper/string.go
@@ -2,7 +2,9 @@ package helper
 
 import (
 	uuid "github.com/satori/go.uuid"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func GetUuidV3(name string) string {
@@ -10,4 +12,20 @@ func GetUuidV3(name string) string {
 	variant := uuid.NewV3(v1, name)
 	all := strings.ReplaceAll(variant.String(), "-", "")
 	return all
+}
+
+func RandString(length int) string {
+	baseStr := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var randStr []byte
+	buf := []byte(baseStr)
+	for i := 0; i < length; i++ {
+		randStr = append(randStr, buf[r.Intn(len(baseStr))])
+	}
+	return string(randStr)
+}
+
+func RandInt(max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(max)
 }
