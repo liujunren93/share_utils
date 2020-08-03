8c59aafb9846d93db4bb8fb093d321188de58572
768712894 <768712894@qq.com>
2020-08-03 22:06:35 +0800

    de

--- error/database.go
+++ error/database.go
@@ -6,13 +6,10 @@ import "github.com/shareChina/utils/helper"
 type dateError struct {
 	Code helper.Status //4004 资源不存在,5000 系统异常 5001 sql异常，其余原样输出
 	Msg  string
-
 }
-
 func (m dateError) Error() string {
 	return m.Msg
 }
-
 //database
 func NewError(code helper.Status, err string) *dateError {
 	return &dateError{Code: code, Msg: err}
