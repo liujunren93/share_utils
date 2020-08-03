b39ea41c124362e8957546c472df4ece963af8d2
768712894 <768712894@qq.com>
2020-08-03 20:49:50 +0800

    de

--- databases/status.go
+++ databases/status.go
@@ -1,21 +1,19 @@
 package databases
 
+import "github.com/shareChina/utils/helper"
+
 type ModelError struct {
-	Code int32 //4004 资源不存在,5000 系统异常 5001 sql异常，其余原样输出
+	Code helper.Status //4004 资源不存在,5000 系统异常 5001 sql异常，其余原样输出
 	Msg  string
 }
 
-var (
-	DataErr             = &ModelError{Code: 5001, Msg: "data error"}
-	InternalServerError = &ModelError{Code: 5000, Msg: "Internal Server Error"}
-	NotFound            = &ModelError{Code: 4004, Msg: "数据不存在"}
-)
+
 
 func (m ModelError) Error() string {
 	return m.Msg
 }
 
-func (Base) NewError(code int32, err string) *ModelError {
+func (Base) NewError(code helper.Status, err string) *ModelError {
 	return &ModelError{Code: code, Msg: err}
 }
 
