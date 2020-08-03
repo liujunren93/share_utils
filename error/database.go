ce7923c806839efe35d9ec9e9d56a8e7c2c546a6
768712894 <768712894@qq.com>
2020-08-03 22:29:12 +0800

    de

--- error/database.go
+++ error/database.go
@@ -3,14 +3,19 @@ package error
 import "github.com/shareChina/utils/helper"
 
 //默认数据错误
-type dateError struct {
+type dataError struct {
 	Code helper.Status //4004 资源不存在,5000 系统异常 5001 sql异常，其余原样输出
 	Msg  string
 }
-func (m dateError) Error() string {
+
+func (m dataError) GetCode() int32 {
+	return int32(m.Code)
+}
+
+func (m dataError) Error() string {
 	return m.Msg
 }
 //database
-func NewError(code helper.Status, err string) *dateError {
-	return &dateError{Code: code, Msg: err}
+func NewError(code helper.Status, err string) *dataError {
+	return &dataError{Code: code, Msg: err}
 }
