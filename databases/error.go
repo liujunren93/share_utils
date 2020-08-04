f39ce8314f32143a5b362a45ff6dab63415b38eb
768712894 <768712894@qq.com>
2020-08-04 10:56:54 +0800

    de

--- databases/error.go
+++ databases/error.go
@@ -2,14 +2,28 @@ package databases
 
 import "github.com/shareChina/utils/helper"
 
+type DBError interface {
+	Code() helper.Status
+	Msg() string
+}
 
+type dbError struct {
+	code helper.Status
+	msg  string
+}
 
-type DBError struct {
-	Code helper.Status
-	Msg  string
+func (d dbError) Code() helper.Status {
+	if d.code == 0 {
+		return 200
+	}
+	return d.code
+}
+
+func (d dbError) Msg() string {
+	return d.msg
 }
 
 //database
-func NewDBError(code helper.Status, err string) *DBError {
-	return &DBError{Code: code, Msg: err}
+func NewDBError(code helper.Status, err string) DBError {
+	return dbError{code: code, msg: err}
 }
