86754de128ea90ee7a8935b151ba81ecb9db7e69
768712894 <768712894@qq.com>
2020-08-04 10:47:14 +0800

    de

--- databases/error.go
+++ databases/error.go
@@ -3,13 +3,13 @@ package databases
 import "github.com/shareChina/utils/helper"
 
 
-//默认数据错误
+
 type DBError struct {
 	Code helper.Status
 	Msg  string
 }
 
 //database
-func NewDBError(code helper.Status, err string) DBError {
-	return DBError{Code: code, Msg: err}
+func NewDBError(code helper.Status, err string) *DBError {
+	return &DBError{Code: code, Msg: err}
 }
