49ee273d8966c4b1cf4ed475e4fd188dbb447f2b
768712894 <768712894@qq.com>
2020-08-06 22:46:10 +0800

    add auth jwt

--- helper/status.go
+++ helper/status.go
@@ -6,6 +6,7 @@ type StatusI interface {
 }
 
 type Status int32
+
 func (s Status) Error() string {
 	return s.GetMsg()
 }
@@ -16,21 +17,23 @@ func (s Status) GetCode() int32 {
 
 func (s Status) GetMsg() (msg string) {
 	switch s {
-		case StatusOK:
-			msg = "ok"
-		case StatusBadRequest:
-			msg = "Request Data Error"
-		case StatusUnauthorized:
-			msg = "Status Unauthorized"
-		case StatusForbidden:
-			msg = "Status Forbidden"
-		case StatusNotFound:
-			msg = "Status Not Found"
-		case StatusInternalServerError:
-			msg = "Status Internal Server Error"
-		case StatusDataError:
-			msg = "status Data Error"
-		default:
+	case StatusOK:
+		msg = "ok"
+	case StatusBadRequest:
+		msg = "Request Data Error"
+	case StatusUnauthorized:
+		msg = "Status Unauthorized"
+	case StatusForbidden:
+		msg = "Status Forbidden"
+	case StatusNotFound:
+		msg = "Status Not Found"
+	case StatusDataDuplication:
+		msg = "Data Duplication"
+	case StatusInternalServerError:
+		msg = "Status Internal Server Error"
+	case StatusDataError:
+		msg = "status Data Error"
+	default:
 
 	}
 	return
@@ -38,13 +41,13 @@ func (s Status) GetMsg() (msg string) {
 
 //database
 
-
 const (
-	StatusOK                  Status = 200//success
+	StatusOK                  Status = 200  //success
 	StatusBadRequest          Status = 4000 //数据绑定错误
 	StatusUnauthorized        Status = 4001 //账户类错误
 	StatusForbidden           Status = 4003 //权限
 	StatusNotFound            Status = 4004 //
+	StatusDataDuplication     Status = 4005 // 数据重复
 	StatusInternalServerError Status = 5000 //服务器未知错误
 	StatusDataError           Status = 5001 //database err
 )
