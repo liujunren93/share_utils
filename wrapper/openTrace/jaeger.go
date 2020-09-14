16e5184d1480adf3d1ce6374b651844126615230
liujunren93 <liujunrenemail@qq.com>
2020-09-14 11:01:08 +0800

    jaeger

--- wrapper/openTrace/jaeger.go
+++ wrapper/openTrace/jaeger.go
@@ -4,35 +4,41 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
+	"google.golang.org/grpc/grpclog"
 	"io"
 	"time"
 )
 
-func NewJaeger(servicename string, addr string) (opentracing.Tracer, io.Closer, error) {
 
+func NewJaeger(serviceName string, jagentHost string) (tracer opentracing.Tracer, closer io.Closer, err error) {
 	cfg := config.Configuration{
-		ServiceName: servicename,
+		ServiceName: serviceName,
 		Sampler: &config.SamplerConfig{
 			Type:  jaeger.SamplerTypeConst,
 			Param: 1,
 		},
-
 		Reporter: &config.ReporterConfig{
 			LogSpans:            true,
 			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  jagentHost,
 		},
 	}
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := log.StdLogger
+	jMetricsFactory := metrics.NullFactory
 
-	sender, err := jaeger.NewUDPTransport(addr, 0)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	reporter := jaeger.NewRemoteReporter(sender)
 	// Initialize tracer with a logger and a metrics factory
-	tracer, closer, err := cfg.NewTracer(
-		config.Reporter(reporter),
-	)
+	tracer, closer, err = cfg.NewTracer(
+		config.Logger(jLogger),
+		config.Metrics(jMetricsFactory))
 
-	return tracer, closer, err
+	if err != nil {
+		grpclog.Errorf("Could not initialize jaeger tracer: %s", err.Error())
+		return
+	}
+	return
 }
