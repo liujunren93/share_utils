af6351ae438fdfa0e516e40f36b0b68d42fcdc59
liujunren93 <liujunrenemail@qq.com>
2020-09-24 17:52:17 +0800

    ua

--- netHelper/respnse.go
+++ netHelper/respnse.go
@@ -21,31 +21,30 @@ type HttpResponse struct {
 	Data interface{}    `json:"data"`
 }
 
-
-//web response
 func Response(w http.ResponseWriter, sta errors.IStatus, err error, data interface{}) {
 	var code int32
 	var msg string
 	if sta != nil {
 		code = sta.GetCode()
 		msg = sta.GetMsg()
-	}
-	if data == nil {
-		if sta != nil {
+		if data != nil {
 			of := reflect.ValueOf(sta)
-
-			if of.Kind()==reflect.Ptr {
+			if of.Kind() == reflect.Ptr {
 				field := of.Elem().FieldByName("Data")
 				if !field.IsZero() {
 					data = field.Interface()
 				}
 			}
-
 		}
 	}
-	if _, ok := status.FromError(err); err != nil && ok {
-		code = 500
-		msg = "Internal Server Error"
+	if e, ok := status.FromError(err); err != nil && ok {
+		if e.Code() == 400 {
+			code = int32(e.Code())
+			msg=e.Message()
+		} else {
+			code = 500
+			msg = "Internal Server Error"
+		}
 	}
 	if err != nil {
 		msg = err.Error()
@@ -63,6 +62,48 @@ func Response(w http.ResponseWriter, sta errors.IStatus, err error, data interfa
 	w.Write(marshal)
 
 }
+
+//web response
+//func Response1(w http.ResponseWriter, sta errors.IStatus, err error, data interface{}) {
+//	var code int32
+//	var msg string
+//	if sta != nil {
+//		code = sta.GetCode()
+//		msg = sta.GetMsg()
+//	}
+//	if data == nil {
+//		if sta != nil {
+//			of := reflect.ValueOf(sta)
+//
+//			if of.Kind() == reflect.Ptr {
+//				field := of.Elem().FieldByName("Data")
+//				if !field.IsZero() {
+//					data = field.Interface()
+//				}
+//			}
+//
+//		}
+//	}
+//	if _, ok := status.FromError(err); err != nil && ok {
+//		code = 500
+//		msg = "Internal Server Error"
+//	}
+//	if err != nil {
+//		msg = err.Error()
+//	}
+//	resData := HttpResponse{
+//		Code: errors.Status(code),
+//		Msg:  msg,
+//		Data: data,
+//	}
+//	if msg != "" {
+//		resData.Msg = msg
+//	}
+//	marshal, _ := json.Marshal(resData)
+//	w.WriteHeader(200)
+//	w.Write(marshal)
+//
+//}
 
 ////通过反射 设置data rpc response
 func RpcResponse(res errors.IStatus, err errors.Error, data interface{}) error {
