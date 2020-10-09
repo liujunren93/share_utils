99f0d1b00731c370da4e753a51a3ec0b9719ddbf
liujunren93 <liujunrenemail@qq.com>
2020-10-09 17:43:43 +0800

    mysqk

--- databases/redis.go
+++ databases/redis.go
@@ -2,12 +2,8 @@ package databases
 
 import "github.com/go-redis/redis"
 
-var RedisDB *redis.Client
-
-
 func NewRedis(conf *redis.Options) *redis.Client {
-	if RedisDB == nil {
-		RedisDB = redis.NewClient(conf)
-	}
-	return RedisDB
+
+	return redis.NewClient(conf)
+
 }
