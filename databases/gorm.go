519aa1bc23bf7b53e1bfe547f5c18dba46d5818c
liujunren93 <liujunrenemail@qq.com>
2020-10-22 16:09:20 +0800

    update mysql

--- databases/gorm.go
+++ databases/gorm.go
@@ -39,10 +39,16 @@ func NewMysql(basConf *Mysql, conf *gorm.Config) (*gorm.DB, error) {
 	if conf == nil {
 		conf = &gorm.Config{NamingStrategy: defaultNamingStrategy}
 	}
-	open, err := gorm.Open(mysql.Open(dsn), conf)
-	if err != nil {
-		return nil, err
-	}
+	open, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,    // DSN data source name
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
+	}), conf)
+
+
 
 	db, err := open.DB()
 	if err != nil {
