d6eb5d1028382f731338d5288d6af084e91532d7
liujunren93 <liujunrenemail@qq.com>
2020-10-27 16:28:20 +0800

    oss

--- pkg/aliyun/_oss/oss.go
+++ pkg/aliyun/_oss/oss.go
@@ -1,12 +1,14 @@
 package _oss
 
 import (
+	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"io"
 )
 
 type _oss struct {
-	client *oss.Client
+	endpoint string
+	client   *oss.Client
 }
 
 //NewOSS
@@ -15,20 +17,20 @@ func NewOSS(endpoint, accessKeyId, secret string) (*_oss, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &_oss{client: client}, err
+	return &_oss{client: client, endpoint: endpoint}, err
 }
 
 //Upload
-func (s *_oss) Upload(objectName, bucketName string, data io.Reader, options ...oss.Option) error {
+func (s *_oss) Upload(objectName, bucketName string, data io.Reader, options ...oss.Option) (string, error) {
 	bucket, err := s.client.Bucket(bucketName)
 	if err != nil {
-		return err
+		return "", err
 	}
 	err = bucket.PutObject(objectName, data, options...)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return fmt.Sprintf("https://%s.%s/%s", bucketName, s.endpoint, objectName), nil
 }
 
 //SignURL
