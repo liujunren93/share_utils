b4a2e1decef9f8f40dc4250da6f48eb0352341ab
liujunren93 <liujunrenemail@qq.com>
2020-11-05 18:04:48 +0800

    config

--- config/store/aliyun.go
+++ config/store/aliyun.go
@@ -12,8 +12,6 @@ type AcmOptions struct {
 	AccessKey   string `json:"access_key"`
 	SecretKey   string `json:"secret_key"`
 	Endpoint    string `json:"endpoint"`
-
-	ServiceName string `json:"service_name"`
 	NamespaceID string `json:"namespace_id"`
 	LogDir      string `json:"log_dir"`
 	CacheDir    string `json:"cache_dir"`
