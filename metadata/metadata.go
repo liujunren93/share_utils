8272c57aca892f2db053de361ff1bcbe38ca522f
liujunren93 <liujunrenemail@qq.com>
2020-12-29 17:12:37 +0800

    randRange

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -7,11 +7,13 @@ import (
 )
 
 type UserAgent struct {
-	CompanyID uint `json:"company_id"` // 机构id
-	AccountID uint `json:"account_id"` // 用户账户id
-	UID       uint `json:"uid"`        // 当前用户ID 服务用户id
-	UserType  int8 `json:"user_type"`  // 1:平台管理员，2：机构内管理员，2：普通用户
-	IsRoot    bool `json:"is_root"`    // 是否是超管
+	CompanyID  uint `json:"company_id"`  // 机构id
+	AccountID  uint `json:"account_id"`  // 用户账户id
+	UID        uint `json:"uid"`         // 当前用户ID 服务用户id
+	UserType   int8 `json:"user_type"`   // 1:平台管理员，2：机构内管理员，2：普通用户
+	IsRoot     bool `json:"is_root"`     // 是否是超管
+	ClientType int8 `json:"client_type"` // 1:平台，2：机构后台，2：客户端
+	ClientID   int8 `json:"client_id"`
 }
 
 //获取
