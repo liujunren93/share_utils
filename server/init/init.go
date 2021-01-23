67db7f377bdc322b495fce9dd8c3c36b5c55b036
liujunren93 <liujunrenemail@qq.com>
2021-01-23 21:41:08 +0800

    server

--- server/init/init.go
+++ server/init/init.go
@@ -4,7 +4,6 @@ import (
 	"github.com/liujunren93/share/core/registry"
 	"github.com/liujunren93/share/core/registry/etcd"
 	"github.com/liujunren93/share/plugins/opentrace"
-	"github.com/liujunren93/share/plugins/validator"
 	"github.com/liujunren93/share/server"
 	"github.com/liujunren93/share_utils/wrapper/openTrace"
 	"github.com/opentracing/opentracing-go"
@@ -16,31 +15,43 @@ type Server struct {
 	Namespace  string
 	OpenTrace  string
 	Weight     int // 权重
+	opts       []server.Option
 }
 
-func (s *Server) NewServer() (*server.GrpcServer, error) {
+func (s *Server) NewServer(opts ...server.Option) (*server.GrpcServer, error) {
+	err := s.initOpts(opts)
+	if err != nil {
+		return nil, err
+	}
 	//注册中心
 	newRegistry, err := etcd.NewRegistry(registry.WithAddrs(s.EtcdAddr...))
 	if err != nil {
 		return nil, err
 	}
-	// 链路
-	jaeger, closer, err := openTrace.NewJaeger(s.ServerName, s.OpenTrace)
-	defer closer.Close()
-	if err != nil {
-		return nil, err
-	}
-	opentracing.SetGlobalTracer(jaeger)
-	var opts []server.Option
-	opts = append(opts, server.WithName(s.ServerName),
-		server.WithHdlrWrappers(opentrace.ServerGrpcWrap(jaeger), validator.NewHandlerWrapper()))
+
 	if s.Namespace != "" {
-		opts = append(opts, server.WithNamespace(s.Namespace))
+		s.opts = append(s.opts, server.WithNamespace(s.Namespace))
 	}
-	grpcServer := server.NewGrpcServer(opts...)
+	grpcServer := server.NewGrpcServer(s.opts...)
 	err = grpcServer.Registry(newRegistry, registry.WithWeight(s.Weight))
 	if err != nil {
 		return nil, err
 	}
 	return grpcServer, nil
+}
+
+func (s *Server) initOpts(opts []server.Option) error {
+
+	// 链路
+	jaeger, closer, err := openTrace.NewJaeger(s.ServerName, s.OpenTrace)
+	defer closer.Close()
+	if err != nil {
+		return err
+	}
+
+	opentracing.SetGlobalTracer(jaeger)
+	s.opts = append(s.opts, server.WithName(s.ServerName),
+		server.WithHdlrWrappers(opentrace.ServerGrpcWrap(jaeger)))
+	s.opts = append(s.opts, opts...)
+	return nil
 }
