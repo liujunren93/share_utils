3165fba091d9ecab9d51e66dd4cc83ef1670d4e1
liujunren93 <liujunrenemail@qq.com>
2021-06-25 23:38:42 +0800

    dev

--- pkg/aliyun/shsts/sts.go
+++ pkg/aliyun/shsts/sts.go
@@ -1,6 +1,7 @@
 package shsts
 
 import (
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/sts"
 	"github.com/liujunren93/share_utils/helper"
 )
@@ -25,11 +26,14 @@ func NewSTS(accessKeyId, secret string) (*STS, error) {
 //@return Credentials 令牌
 //@return string sessionName
 //@return error
-func (s *STS) Credentials(roleArn, session string) (*sts.Credentials, string, error) {
+func (s *STS) Credentials(roleArn, session string, expire int) (*sts.Credentials, string, error) {
 	//构建请求对象。
 	request := sts.CreateAssumeRoleRequest()
 	request.Scheme = "https"
-
+	if expire < 900 {
+		expire = 900
+	}
+	request.DurationSeconds = requests.NewInteger(expire)
 	//设置参数。关于参数含义和设置方法，请参见API参考。
 
 	request.RoleArn = roleArn
@@ -42,6 +46,6 @@ func (s *STS) Credentials(roleArn, session string) (*sts.Credentials, string, er
 	if err != nil {
 		return nil, "", err
 	}
-	return &role.Credentials,session, nil
+	return &role.Credentials, session, nil
 
 }
