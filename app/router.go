9c518b93efa35ff6d422a68fab62a4f4f003c32d
liujunren93 <liujunrenemail@163.com>
2022-08-30 18:50:13 +0800

    redis client

--- app/router.go
+++ app/router.go
@@ -2,16 +2,6 @@ package app
 
 import "github.com/liujunren93/share_utils/databases/redis"
 
-type Router struct {
-	Method    string                 `json:"method" yaml:"method"`
-	ReqParams map[string]interface{} `json:"req_params" yaml:"req_params"`
-}
-
-type RouterCenter interface {
-	GetRouter() map[string]Router //map[method:path]map[]
-	Registry(map[string]Router) error
-}
-
 func (app *App) initRouter() {
 	if !app.LocalConf.EnableAutoRoute {
 		return
--- databases/redis/redis.go
+++ databases/redis/redis.go
@@ -3,29 +3,11 @@ package redis
 import (
 	"context"
 	"time"
-
-	re "github.com/go-redis/redis/v8"
 )
 
-func NewRedis(conf *Config) (*re.Client, error) {
-
-	opt := re.Options{
-		Network:  conf.Network,
-		Addr:     conf.Addr,
-		Username: conf.Username,
-		Password: conf.Password,
-		DB:       conf.DB,
+func (c *Client) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) {
+	if c.Mode == 0 {
+		c.Client.Set(ctx, key, value, expiration)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-	cli := re.NewClient(&opt)
-	err := cli.Ping(ctx).Err()
-	return cli, err
-}
-
-type Config struct {
-	Network  string `js`
-	Addr     string
-	Username string
-	Password string
-	DB       int
+	c.ClusterClient.Set(ctx, key, value, expiration)
 }
